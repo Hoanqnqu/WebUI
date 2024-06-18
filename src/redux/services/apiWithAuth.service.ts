4ebaef19ff5ab8e5aea617ffed2441384d6a14f3
Nhat Hoang <nhathoangthuychaub1@gmail.com>
2024-06-18 19:25:50 +0700

    fix

--- src/redux/services/apiWithAuth.service.ts
+++ src/redux/services/apiWithAuth.service.ts
@@ -5,14 +5,15 @@ import { BASE_URL } from "@/utils/constants/GlobalConst"
 const baseQuery = fetchBaseQuery({
     baseUrl: BASE_URL,
     // credentials: 'include',
-    mode: "cors",
+    // mode: "cors",
 
     prepareHeaders: (headers, { getState }) => {
         // headers.set("Accept", "true")
-        // headers.set("Content-Type", "application/json")
-        headers.set("Access-Control-Allow-Origin", "*")
+        headers.set("Content-Type", "application/json")
+        // headers.set("Access-Control-Allow-Origin", "*")
         // headers.set("Access-Control-Allow-Credentials", "true")
-        headers.set("Cross-Origin-Opener-Policy", "same-origin-allow-popups")
+        // headers.set("Access-Control-Allow-Headers", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
+        headers.set("Cross-Origin-Opener-Policy", "unsafe-none")
         const token = (getState() as RootState).auth.accessToken
         if (token) {
             headers.set("authorization", `Bearer ${token}`)
